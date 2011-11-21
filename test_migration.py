d353e70a7251d649e10380498b4f4785ea7e49e9
Sven A. Schmidt <sas@abstracture.de>
2011-11-21 16:10:38 +0100

    Added level attribute for migration

--- test_migration.py
+++ test_migration.py
@@ -1,16 +1,25 @@
 import unittest
 import couchdb
+import random
 
 URL='http://localhost:5984'
 DBNAME = 'migration'
 
 
 def make_player(name, version):
-  return {
+  player = {
     'type':'player',
     'version':version,
     'name:':name,
+    'xp':int(1000*random.random())
   }
+  if version == 1:
+    return player
+  elif version == 2:
+    player['level'] = player['xp']/100 + 1
+  elif version == 3:
+    player['level'] = player['xp']/200 + 1
+  return player
 
 
 def migrate_v1_v2(db):
@@ -19,7 +28,8 @@ def migrate_v1_v2(db):
     doc = db[row.key]
     if doc['version'] == 1:
       doc['version'] = 2
-      # change other attributes...
+      # we want to add the level stat, which is simply xp/100, starting from 1
+      doc['level'] = doc['xp']/100 + 1
       db[doc.id] = doc
 
 
@@ -29,7 +39,9 @@ def migrate_v2_v3(db):
     doc = db[row.key]
     if doc['version'] == 2:
       doc['version'] = 3
-      # change other attributes...
+      # ok, we found folks are leveling too fast, so we recompute all levels
+      # from existing xp values, players are going to love this!
+      doc['level'] = doc['xp']/200 + 1
       db[doc.id] = doc
 
 
@@ -86,10 +98,18 @@ class TestMigration(unittest.TestCase):
     migrate_v1_v2(self.db)
     v1 = self.db.view('_design/migration/_view/v1')
     self.assertEqual(0, len(v1.rows))
+
     v2 = self.db.view('_design/migration/_view/v2')
     self.assertEqual(9, len(v2.rows))
+    for row in v2.rows:
+      doc = self.db[row.key]
+      self.assertEqual(doc['xp']/100 + 1, doc['level'])
+    
     v3 = self.db.view('_design/migration/_view/v3')
     self.assertEqual(6, len(v3.rows))
+    for row in v3.rows:
+      doc = self.db[row.key]
+      self.assertEqual(doc['xp']/200 + 1, doc['level'])
 
 
   def test_03_migrate_v2_v3(self):
