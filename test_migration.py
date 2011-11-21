ae2b7660008879c9b1865356ca5b70e346db22da
Sven A. Schmidt <sas@abstracture.de>
2011-11-21 15:31:33 +0100

    Added migration

--- test_migration.py
+++ test_migration.py
@@ -13,6 +13,26 @@ def make_player(name, version):
   }
 
 
+def migrate_v1_v2(db):
+  v1 = db.view('_design/migration/_view/v1')
+  for row in v1.rows:
+    doc = db[row.key]
+    if doc['version'] == 1:
+      doc['version'] = 2
+      # change other attributes...
+      db[doc.id] = doc
+
+
+def migrate_v2_v3(db):
+  v2 = db.view('_design/migration/_view/v2')
+  for row in v2.rows:
+    doc = db[row.key]
+    if doc['version'] == 2:
+      doc['version'] = 3
+      # change other attributes...
+      db[doc.id] = doc
+
+
 class TestMigration(unittest.TestCase):
 
   def setUp(self):
@@ -61,6 +81,36 @@ class TestMigration(unittest.TestCase):
     v3 = self.db.view('_design/migration/_view/v3')
     self.assertEqual(6, len(v3.rows))
 
+
+  def test_02_migrate_v1_v2(self):
+    migrate_v1_v2(self.db)
+    v1 = self.db.view('_design/migration/_view/v1')
+    self.assertEqual(0, len(v1.rows))
+    v2 = self.db.view('_design/migration/_view/v2')
+    self.assertEqual(9, len(v2.rows))
+    v3 = self.db.view('_design/migration/_view/v3')
+    self.assertEqual(6, len(v3.rows))
+
+
+  def test_03_migrate_v2_v3(self):
+    migrate_v2_v3(self.db)
+    v1 = self.db.view('_design/migration/_view/v1')
+    self.assertEqual(4, len(v1.rows))
+    v2 = self.db.view('_design/migration/_view/v2')
+    self.assertEqual(0, len(v2.rows))
+    v3 = self.db.view('_design/migration/_view/v3')
+    self.assertEqual(11, len(v3.rows))
+
+
+  def test_03_migrate_v1_v3(self):
+    migrate_v1_v2(self.db)
+    migrate_v2_v3(self.db)
+    v1 = self.db.view('_design/migration/_view/v1')
+    self.assertEqual(0, len(v1.rows))
+    v2 = self.db.view('_design/migration/_view/v2')
+    self.assertEqual(0, len(v2.rows))
+    v3 = self.db.view('_design/migration/_view/v3')
+    self.assertEqual(15, len(v3.rows))
 
 
 if __name__=='__main__':
